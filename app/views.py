d55ec0041c600ee5538f82094541f3078f84659b
watchpenguin <watchpenguin@gmail.com>
2015-05-22 22:35:20 +0800

    Simple of Forms&Openid

--- app/views.py
+++ app/views.py
@@ -1,21 +1,33 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
+
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Mingming'}
+    user = {'nickname' : 'Ming'}
     posts = [
         {
-            'author' : {'nickname': 'Ming'},
-            'body' : 'Beautiful day in Portland!'
+            'author': {'nickname': 'Ming'},
+            'body' : 'Beautiful day in Suzhou!'
         },
         {
-            'author' : {'nickname': 'Wang'},
+            'author': {'nickname': 'ming'},
             'body' : 'The Avengers movie was so cool!'
         }
     ]
-
     return render_template("index.html",
-        title = "Home",
+        title = 'Home',
         user = user,
         posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
