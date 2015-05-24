5c85b3df53fc5f8bf6839e057ebcb7ceb56cdb5f
watchpenguin <watchpenguin@gmail.com>
2015-05-24 23:17:22 +0800

    Add Error Handling

--- app/views.py
+++ app/views.py
@@ -125,6 +125,14 @@ def edit():
 
 
 
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
 
 
 
--- config.py
+++ config.py
@@ -14,3 +14,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+#### Mail server setting ####
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+ADMINS = ['watchpenguin@gmail.com']
