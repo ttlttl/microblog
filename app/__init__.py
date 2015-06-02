3bd9ffd9df8696af2363ee1ab0a164386d1717af
watchpenguin <watchpenguin@gmail.com>
2015-06-02 22:41:30 +0800

    Add utctime support

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,11 @@ from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 from flask.ext.mail import Mail
+from momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.jinja_env.globals['momentjs'] = momentjs
 db = SQLAlchemy(app)
 
 mail = Mail(app)
